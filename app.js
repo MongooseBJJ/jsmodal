8437187b70489356d862a36bcefd86d0b5a97642
MongooseBJJ <nkgwebdev@gmail.com>
2022-09-10 18:22:05 -0500

    fix: btn logic

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const modalBtn = document.querySelector('modal-btn');
-const closeBtn = document.querySelector('close-btn');
-const modal = document.querySelector('modal-overlay');
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closebtn = document.querySelector(".close-btn");
 
-modalBtn.addEventListener('click', function() {
-    modal.classList.add('open-modal');
+modalBtn.addEventListener("click", function() {
+    modal.classList.add("open-modal");
 });
-
-closeBtn.addEventListener('click', function() {
-    modal.classList.remove('open-modal');
+closebtn.addEventListener("click", function() {
+    modal.classList.remove("open-modal");
 });
